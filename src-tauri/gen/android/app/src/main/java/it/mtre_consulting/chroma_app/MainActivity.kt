8353999676a1c1fcabf7aef4a74f4dd505670537
m4ce-w1ndu <16584672+m4ce-w1ndu@users.noreply.github.com>
2026-03-30 11:39:08 +0200

    Added Material-3 style icons to Navbar

--- src-tauri/gen/android/app/src/main/java/it/mtre_consulting/chroma_app/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/it/mtre_consulting/chroma_app/MainActivity.kt
@@ -1,11 +1,40 @@
 package it.mtre_consulting.chroma_app
 
+import android.os.Build
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+  }
+
+  override fun onWebViewCreate(webView: WebView) {
+    super.onWebViewCreate(webView)
+
+    // Material You dynamic colors are only available on Android 12 (API 31) and up
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        // Fetch the system accent color
+        val systemAccent = resources.getColor(android.R.color.system_accent1_500, theme)
+        
+        // Convert to a hex string that CSS can read
+        val hexAccent = String.format("#%06X", 0xFFFFFF and systemAccent)
+
+        // Inject a style block into the document head
+        val js = """
+            const style = document.createElement('style');
+            style.innerHTML = `
+                :root {
+                    --accent: $hexAccent !important;
+                }
+            `;
+            document.head.appendChild(style);
+        """.trimIndent()
+
+        // Evaluate the JS in the webview
+        webView.evaluateJavascript(js, null)
+    }
   }
 }
