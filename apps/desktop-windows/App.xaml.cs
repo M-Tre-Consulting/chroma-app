3ad3cc495c3765b94f20b70d12df47a627debb09
Simone Rolando <quark@immauristation.localdomain>
2026-06-01 03:13:12 +0200

    style(desktop-windows): implement global dark theme brushes and control templates

---
+++ apps/desktop-windows/App.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace Chroma
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
